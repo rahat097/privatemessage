83da2ab01269a34baa76628c0f9fac1f72d5278b
rahat097 <31737891+rahat097@users.noreply.github.com>
2021-01-20 17:12:04 +0600

    Add files via upload

--- context_processors.py
+++ context_processors.py
@@ -4,9 +4,12 @@ from . models import PrivateMessage
 
 def add_variable_to_context(request):
 	if request.user.is_authenticated:
+		lastmessage = request.user.privatemessage.filter(is_seen = False).last()
 		unreadmessagecount = PrivateMessage.objects.filter(pmreciever = request.user, is_seen = False).count()
 	else:
+		lastmessage = 0
 		unreadmessagecount = 0
 	return {
-        'unreadmessagecount': unreadmessagecount
+        'unreadmessagecount': unreadmessagecount,
+        'lastmessage' : lastmessage
     }
